caef75ca966cc5bdfdbf5b5490367d1c5f4f5ec8
Blake Mills <blakem30@yahoo.com>
2011-03-10 10:22:56 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CuphonApp::Application.config.secret_token = 'df6d0c2c72f6bdc18cf43dd6c53814b3642ed0d1e3f956bfae329e3d2b647cca42bf6911b30230097ef4c4c7e8134d776f87b2bd0cff85bb2f30e075099f8f57'
