32681b0473195c61b9043387a5f48c7f839b31a0
Blake Mills <blakem30@yahoo.com>
2011-03-11 15:01:44 -0800

    minor change for live test

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 I_KNOW_I_AM_USING_AN_OLD_AND_BUGGY_VERSION_OF_LIBXML2 = 1
-ENV['DATABASE_URL'] = 'mysql://root:;[sGueRsr1_Fvc_a[vB/@db.cuphon.dotcloud.com:1083'
+ENV['DATABASE_URL'] = 'mysql://root:;[sGueRsr1_Fvc_a[vB/@db.cuphon.dotcloud.com:1083/cuphon_production'
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
