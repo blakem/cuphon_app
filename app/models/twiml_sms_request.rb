34353675863d6cde388b3b73e3266fd2980424d0
Blake Mills <blakem30@yahoo.com>
2011-03-10 23:02:41 -0800

    minor change for live test

--- app/models/twiml_sms_request.rb
+++ app/models/twiml_sms_request.rb
@@ -1,20 +1,20 @@
 class TwimlSmsRequest < ActiveRecord::Base
   
-  def self.new_from_params(params)
-    self.new(
-     :AccountSid   => params[:AccountSid],
-     :Body         => params[:Body],
-     :From         => params[:From],
-     :FromCity     => params[:FromCity],
-     :FromCountry  => params[:FromCountry],
-     :FromState    => params[:FromState],
-     :FromZip      => params[:FromZip],
-     :SmsSid       => params[:SmsSid],
-     :To           => params[:To],
-     :ToCity       => params[:ToCity],
-     :ToCountry    => params[:ToCountry],
-     :ToState      => params[:ToState],
-     :ToZip        => params[:ToZip],
-    )
-  end
+  # def self.new_from_params(params)
+  #   self.new(
+  #    :AccountSid   => params[:AccountSid],
+  #    :Body         => params[:Body],
+  #    :From         => params[:From],
+  #    :FromCity     => params[:FromCity],
+  #    :FromCountry  => params[:FromCountry],
+  #    :FromState    => params[:FromState],
+  #    :FromZip      => params[:FromZip],
+  #    :SmsSid       => params[:SmsSid],
+  #    :To           => params[:To],
+  #    :ToCity       => params[:ToCity],
+  #    :ToCountry    => params[:ToCountry],
+  #    :ToState      => params[:ToState],
+  #    :ToZip        => params[:ToZip],
+  #   )
+  # end
 end
