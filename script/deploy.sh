99916ed51d1233c2e778baf09a33d125ae75ca2b
Blake Mills <blakem30@yahoo.com>
2011-03-12 21:13:46 -0800

    minor change for live test

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,4 +3,8 @@ git push heroku
 echo "Heroku Done....."
 git push beanstalk
 dotcloud push cuphonapi.frontend .
-dotcloud run  cuphonapi.frontend 'cd code; rake db:migrate'
+# dotcloud run  cuphonapi.frontend 'cd code; rake db:migrate'
+RAILS_ENV=test rake db:migrate
+RAILS_ENV=development rake db:migrate
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=localtest rake db:migrate
