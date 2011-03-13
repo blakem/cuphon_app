481364e92f02fc20264d94b65a4641ab92e4e01f
Blake Mills <blakem30@yahoo.com>
2011-03-12 17:15:15 -0800

    minor change for live test

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,4 @@ git push heroku
 echo "Heroku Done....."
 git push beanstalk
 dotcloud push cuphonapi.frontend .
-
-echo "You may need to 'dotcloud ssh cuphonapi.frontend     cd code; rake db:migrate; exit"
+dotcloud run  cuphonapi.frontend 'cd code; rake db:migrate'
