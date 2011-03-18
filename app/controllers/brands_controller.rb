c94da517f3ad022bd0b74b75787ee51927d0cba4
Blake Mills <blakem30@yahoo.com>
2011-03-18 00:03:24 -0700

    Make brand sort work ASCending

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,13 +5,13 @@ class BrandsController < ApplicationController
     sort_string = get_sort_string(params[:sort])
     @title = 'Brands'
     @brands = Brand.find(:all, :order => sort_string).paginate(:page => params[:page], :per_page => 100)
-    @sort_args={:brand => {:sort => params[:sort] == 'brand_desc' ? 'brand_inc' : 'brand_desc'}}
+    @sort_args={:brand => {:sort => params[:sort] == 'brand_asc' ? 'brand_desc' : 'brand_asc'}}
   end
 
   private
     def get_sort_string(key)
       {'brand_desc' => 'title desc',
-       'brand' => 'title'      
+       'brand_asc' => 'title asc'      
       }[key] || 'id desc'
     end
 end
