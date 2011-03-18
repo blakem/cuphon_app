5b7c6aea1f8f5f50c69683368184093690b52095
Blake Mills <blakem30@yahoo.com>
2011-03-18 12:23:59 -0700

    Sort brands list by created_at

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,13 +5,16 @@ class BrandsController < ApplicationController
     sort_string = get_sort_string(params[:sort])
     @title = 'Brands'
     @brands = Brand.find(:all, :order => sort_string).paginate(:page => params[:page], :per_page => 100)
-    @sort_args={:brand => {:sort => params[:sort] == 'brand_asc' ? 'brand_desc' : 'brand_asc'}}
+    @sort_args={:brand => {:sort => params[:sort] == 'brand_asc' ? 'brand_desc' : 'brand_asc'},
+                :created_at => {:sort => params[:sort] == 'created_at_asc' ? 'created_at_desc' : 'created_at_asc'}}
   end
 
   private
     def get_sort_string(key)
       {'brand_desc' => 'title desc',
-       'brand_asc' => 'title asc'      
+       'brand_asc' => 'title asc',
+       'created_at_desc' => 'created_at desc',
+       'created_at_asc' => 'created_at asc'
       }[key] || 'id desc'
     end
 end
