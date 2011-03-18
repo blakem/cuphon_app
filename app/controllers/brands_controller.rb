e4aafc4b6404b1c6a82e5e268a5d4d4b35ce27d8
Blake Mills <blakem30@yahoo.com>
2011-03-18 12:53:34 -0700

    Sort brands list by subscriber count

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,19 +2,24 @@ class BrandsController < ApplicationController
   before_filter :authenticate_user!
 
   def list
-    sort_string = get_sort_string(params[:sort])
+    sort_args = get_sort_args(params[:sort])
     @title = 'Brands'
-    @brands = Brand.find(:all, :order => sort_string).paginate(:page => params[:page], :per_page => 100)
+
+#    Book.find(:all, :select => "author_id, count(id) as book_count", :group => "author_id", :order => "book_count")
+    @brands = Brand.find(:all, sort_args).paginate(:page => params[:page], :per_page => 100)
     @sort_args={:brand => {:sort => params[:sort] == 'brand_asc' ? 'brand_desc' : 'brand_asc'},
-                :created_at => {:sort => params[:sort] == 'created_at_asc' ? 'created_at_desc' : 'created_at_asc'}}
+                :created_at => {:sort => params[:sort] == 'created_at_asc' ? 'created_at_desc' : 'created_at_asc'},
+                :subscriber => {:sort => params[:sort] == 'subscriber_asc' ? 'subscriber_desc' : 'subscriber_asc'}}
   end
 
   private
-    def get_sort_string(key)
-      {'brand_desc' => 'title desc',
-       'brand_asc' => 'title asc',
-       'created_at_desc' => 'created_at desc',
-       'created_at_asc' => 'created_at asc'
-      }[key] || 'id desc'
+    def get_sort_args(key)
+      {'brand_desc' =>      {:order =>'title desc'},
+       'brand_asc' =>       {:order => 'title asc'},
+       'created_at_desc' => {:order => 'created_at desc'},
+       'created_at_asc' =>  {:order => 'created_at asc'},
+       'subscriber_desc' => {:order => '(SELECT COUNT(*) FROM subscriptions WHERE (brands.id=subscriptions.brand_id)) desc'},
+       'subscriber_asc' => {:order => '(SELECT COUNT(*) FROM subscriptions WHERE (brands.id=subscriptions.brand_id)) asc'}
+      }[key] || {:order => 'id desc'}
     end
 end
